c00928802e48fca7bab46aef6bef24e6769ddfbc
user131309 <user131309@ny174.local>
2017-11-07 12:32:01 -0500

    Adding prototype cell identifier to FeedViewController

--- SocialApp 7.3/FeedViewController.swift
+++ SocialApp 7.3/FeedViewController.swift
@@ -17,8 +17,8 @@ class FeedViewController: UITableViewController {
     var tweets : NSMutableArray?
     var imageCache : NSCache<AnyObject, AnyObject>?
     var queue : OperationQueue?
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = selectedAccount.accountDescription
@@ -36,108 +36,108 @@ class FeedViewController: UITableViewController {
                 URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
             
             if let request = SLRequest(forServiceType: SLServiceTypeTwitter,
-                                    requestMethod: SLRequestMethod.GET,
-                                    url: requestURL,
-                                    parameters: [:]){
-            
-            request.account = account
-            request.perform() {
-                responseData, urlResponse, error in
+                                       requestMethod: SLRequestMethod.GET,
+                                       url: requestURL,
+                                       parameters: [:]){
                 
-                if(urlResponse?.statusCode == 200) {
+                request.account = account
+                request.perform() {
+                    responseData, urlResponse, error in
+                    
                     if(urlResponse?.statusCode == 200) {
-                        do{
-                            self.tweets = try JSONSerialization.jsonObject(with: responseData!, options: JSONSerialization.ReadingOptions.mutableContainers
-                                ) as? NSMutableArray
-
+                        if(urlResponse?.statusCode == 200) {
+                            do{
+                                self.tweets = try JSONSerialization.jsonObject(with: responseData!, options: JSONSerialization.ReadingOptions.mutableContainers
+                                    ) as? NSMutableArray
+                                
+                            }
+                            catch let error as NSError{
+                                print ("json eror: \(error.localizedDescription)")
+                            }
                         }
-                        catch let error as NSError{
-                            print ("json eror: \(error.localizedDescription)")
-                        }
+                    }
+                    
+                    DispatchQueue.main.async() {
+                        self.tableView.reloadData()
                     }
                 }
                 
-                DispatchQueue.main.async() {
-                    self.tableView.reloadData()
-                }
             }
-            
         }
-    }
-
- 
-
+        
+        
+        
         func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        // MARK: - Table view data source
+        
         func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+            // #warning Incomplete implementation, return the number of sections
+            return 0
+        }
+        
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+            // #warning Incomplete implementation, return the number of rows
+            return 0
+        }
+        
+        /*
+         override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+         
+         // Configure the cell...
+         
+         return cell
+         }
+         */
+        
+        /*
+         // Override to support conditional editing of the table view.
+         override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+         // Return false if you do not want the specified item to be editable.
+         return true
+         }
+         */
+        
+        /*
+         // Override to support editing the table view.
+         override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+         if editingStyle == .Delete {
+         // Delete the row from the data source
+         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+         } else if editingStyle == .Insert {
+         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+         }
+         }
+         */
+        
+        /*
+         // Override to support rearranging the table view.
+         override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+         
+         }
+         */
+        
+        /*
+         // Override to support conditional rearranging of the table view.
+         override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+         // Return false if you do not want the item to be re-orderable.
+         return true
+         }
+         */
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+         // Get the new view controller using segue.destinationViewController.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
 }
